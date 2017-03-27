7aea9a314eba419410261875794b6e9902d33bef
Acellam Guy <abiccel@yahoo.com>
2017-03-27 15:00:32 +0300

    Fix constructor issue for mx7

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist/tmp"),
         filename: "src/com/mendix/widget/StarRating/StarRating.js",
-        libraryTarget:  "umd",
-        umdNamedDefine: true,
-        library: "com.mendix.widget.StarRating.StarRating"
+        libraryTarget:  "umd"
     },
     resolve: {
         extensions: [ "", ".ts", ".js", ".jsx", ".json" ],
